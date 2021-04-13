52d5ba757f5bb2f93fc889910b033cd83ffe3c06
Amal Shaiju <65841904+AmalShaiju@users.noreply.github.com>
2021-04-13 14:25:43 -0400

    Add project files.

---
+++ AnimeNorth/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimeNorth
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            var nav = new NavigationPage(new Page());
+            MainPage = nav;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
